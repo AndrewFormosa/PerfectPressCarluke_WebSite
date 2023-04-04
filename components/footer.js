36e9df9e81d5d4778178399635f490764cda1749
AndrewFormosa <andyformosa@aol.com>
2023-04-04 13:18:57 +0100

    change open hours

--- components/footer.js
+++ components/footer.js
@@ -7,7 +7,7 @@ class Footer extends HTMLElement {
         this.innerHTML = `
         <div class="footer">
         <div class="footer_text">
-         Perfect Press | Tel: 01555 770992 | email: mahamrah@yahoo.co.uk | Website by AJF
+         Perfect Press | Tel: 01555 770992 | email: info@perfectpresscarluke.co.uk | Website by AJF
         </div>
       </div>
         `;
