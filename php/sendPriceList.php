15b6c9da76367295c3f19d3789877ddd6b2b4e74
Andrew Formosa <andyformosa@aol.com>
2023-04-07 16:12:19 +0100

    addtoform

---
+++ php/sendPriceList.php
@@ -0,0 +1,28 @@
+<?php
+$customer = $_POST["customer_name"];
+$contact = $_POST["customer_contact"];
+
+$email_to = "info@perfectpresscarluke.co.uk";
+$name = "Andy James";
+$email_from = "info@perfectpresscarluke.co.uk";
+
+$reply_to = "info@perfectpresscarluke.co.uk";
+$message = "Please send a price list to :" .$customer ."\nCustomer Email Address is: " .$contact ."\nFrom your website";
+
+$email_subject = "Price list request";
+$headers = "From: " . $email_from . "\n";
+$headers .= "Reply-To: " . $reply_to . "\n";
+
+echo $message;
+
+ini_set("sendmail_from", $email_from);
+$sent = mail($email_to, $email_subject, $message, $headers, "-f" .$email_from);
+if ($sent)
+{
+echo "sent successful";
+} else {
+echo "There has been an error sending your comments. Please try later.";
+}
+header("Location:http://www.perfectpresscarluke.co.uk/thankyou.html");
+exit();
+?>
